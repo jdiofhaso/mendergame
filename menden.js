6cc62b9de5d3c9f369715f005617dbb6d9eec6cc
jdiofhaso <66395711+jdiofhaso@users.noreply.github.com>
2023-09-19 12:23:35 -0400

    Update menden.js

--- menden.js
+++ menden.js
@@ -1,29 +1,34 @@
 let rand
 let words
+var wordchoices = ["farted","mendenmomered","need help", "phated out my shenie","shated out my penie","penied my mendene","subbed 2 faard","yor momerd",'helppp meeeeeeeeeee they are in my wallssssssssss'
+                   ]
 
 function mousePressed() {
-  let rand = Math.floor(Math.random());
+  let rand = Math.floor(Math.random()*wordchoices.length);
   };
-if(rand=0) {
-  let words = 'fog!!!'
-} else if(rand=1) {
-  let words = 'did a Menden Momer.'
-  } else if(rand=2) {
-    let words = 'sub2faard'
-    } else if(rand=3) {
-      let words = 'fart ganged extra big'
-      } else if(rand=4) {
-        let words = 'yor mom best youtuber!!!'
-        } else  if(rand=5) {
-          let words = 'fogged'
-          } else  if(rand=6) {
-              let words = 'wanted mender game 2 coming soon'
-              } else  if(rand=7) {
-                  let words = 'fart ganged'
-                  } else  if(rand=8) {
-                      let words = 'collabed with robert tobert'
-                      } else  if(rand=9) {
-                          let words = 'jaropee momerod !!!!!'
-                          };
+let words = wordchoices[rand]
+
+
+// if(rand=0) {
+//   let words = 'fog!!!'
+// } else if(rand=1) {
+//   let words = 'did a Menden Momer.'
+//   } else if(rand=2) {
+//     let words = 'sub2faard'
+//     } else if(rand=3) {
+//       let words = 'fart ganged extra big'
+//       } else if(rand=4) {
+//         let words = 'yor mom best youtuber!!!'
+//         } else  if(rand=5) {
+//           let words = 'fogged'
+//           } else  if(rand=6) {
+//               let words = 'wanted mender game 2 coming soon'
+//               } else  if(rand=7) {
+//                   let words = 'fart ganged'
+//                   } else  if(rand=8) {
+//                       let words = 'collabed with robert tobert'
+//                       } else  if(rand=9) {
+//                           let words = 'jaropee momerod !!!!!'
+//                           };
 
 document.write('i just' + words);
