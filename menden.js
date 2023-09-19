47c2c3379dcb17690089338ff78d246db4301633
jdiofhaso <66395711+jdiofhaso@users.noreply.github.com>
2023-09-19 10:35:16 -0400

    Update menden.js

--- menden.js
+++ menden.js
@@ -7,23 +7,23 @@ function mousePressed() {
 if(rand=0) {
   let words = 'fog!!!'
 } else if(rand=1) {
-  let words = 'Menden Momer.'
+  let words = 'did a Menden Momer.'
   } else if(rand=2) {
     let words = 'sub2faard'
     } else if(rand=3) {
-      let words = 'fart gang'
+      let words = 'fart ganged extra big'
       } else if(rand=4) {
         let words = 'yor mom best youtuber!!!'
         } else  if(rand=5) {
-          let words = 'When will the fog get here?'
+          let words = 'fogged'
           } else  if(rand=6) {
-              let words = 'mender game 2 coming soon'
+              let words = 'wanted mender game 2 coming soon'
               } else  if(rand=7) {
-                  let words = 'fart gang'
+                  let words = 'fart ganged'
                   } else  if(rand=8) {
-                      let words = 'collab with robert tobert'
+                      let words = 'collabed with robert tobert'
                       } else  if(rand=9) {
-                          let words = 'jaropee momero !!!!!'
+                          let words = 'jaropee momerod !!!!!'
                           };
 
-document.write(<h2>words</h2>);
+document.write('i just' + words);
